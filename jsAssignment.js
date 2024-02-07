9b7c040c1076412442e8717968e353a5bed03458
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-02-07 23:52:43 +0600

    first commit

---
+++ jsAssignment.js
@@ -0,0 +1,15 @@
+function checkName(name) {
+  if (typeof name !== "string") {
+    return "invalid";
+  } else {
+    const goodNameLastChr = ["a", "y", "i", "e", "o", "u", "w"];
+    const givenNameLastChr = name.slice(-1).toLowerCase();
+    if (goodNameLastChr.includes(givenNameLastChr)) {
+      return "Good Name";
+    } else {
+      return "Bad Name";
+    }
+  }
+}
+
+console.log(checkName("Salman"));
